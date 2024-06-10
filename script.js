0cf059e4e9e0a6ab990819df1b303d3fa593e747
Justin Ken <justinken.ramoso@gmail.com>
2024-06-10 15:51:37 +0800

    It is now possible to change grid size

--- script.js
+++ script.js
@@ -4,21 +4,39 @@ const TEAL = "#00ADB5";
 const OFFWHITE = "#EEEEEE";
 
 const canvas = document.querySelector(".canvas");
-let gridSize = 16;
 
-for (let row = 0; row < gridSize; row ++) {
+function generateGrid(size) {
 
-    const newRow = document.createElement("div");
-    newRow.setAttribute("class", "row")
-    canvas.appendChild(newRow)
+    for (let row = 0; row < size; row ++) {
 
-    for (let i = 0; i < gridSize; i ++) {
-        const square = document.createElement("div");
-        square.setAttribute("class", "square");
-        square.addEventListener("mouseover", (e) => {
-            square.style.backgroundColor = TEAL;
-        });
-        newRow.appendChild(square);
-    }
+        const newRow = document.createElement("div");
+        newRow.setAttribute("class", "row")
+        canvas.appendChild(newRow)
     
+        for (let i = 0; i < size; i ++) {
+            const square = document.createElement("div");
+            square.setAttribute("class", "square");
+            square.addEventListener("mouseover", (e) => {
+                square.style.backgroundColor = TEAL;
+            });
+            newRow.appendChild(square);
+        }
+        
+    }
 }
+
+
+const gridBtn = document.querySelector("#grid-btn");
+gridBtn.addEventListener("click", () => {
+    const newGridSize = parseInt(prompt("Set new grid size (max: 64):"));
+
+    if (newGridSize > 0 && newGridSize < 65) {
+        canvas.innerHTML = "";
+        generateGrid(newGridSize);
+    } else {
+        alert("Please input a value from 1 to 64.")
+    }
+
+})
+
+generateGrid(16);
